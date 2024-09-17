7737cca8e2411e7ce6d74bab971b8aef56335a58
korsNaike <kirillgrinev2003@mail.ru>
2024-09-17 19:30:55 +0300

    Л1.З4. Убрал лишнее при перезаписи видео

--- opencv_lab/open_cv_task4.py
+++ opencv_lab/open_cv_task4.py
@@ -1,42 +1,29 @@
 import cv2
 
-# Открываем исходное видео
-input_video = cv2.VideoCapture('files/video.mp4')
+# Название входного файла
+input_file = "files/output.mp4"
+# Название выходного файла
+output_file = "files/rerecord_video.mp4"
 
-# Получаем информацию о видео
-fps = input_video.get(cv2.CAP_PROP_FPS)
-width = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-total_frames = input_video.get(cv2.CAP_PROP_FRAME_COUNT)
+# Открываем входной видеофайл
+cap = cv2.VideoCapture(input_file)
 
-# Создаем объект для записи выходного видео
+# Получаем параметры входного видео
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Создаем выходной видеофайл
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-output_video = cv2.VideoWriter('files/gray_video.mp4', fourcc, fps, (width, height), isColor=False)
+out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
 
-# Отображаем и записываем видео
+# Читаем и записываем кадры
 while True:
-    # Считываем кадр
-    ret, frame = input_video.read()
-
-    # Если кадр не удалось считать, выходим из цикла
+    ret, frame = cap.read()
     if not ret:
         break
-
-    # Конвертируем кадр в оттенки серого
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Записываем кадр в выходное видео
-    output_video.write(gray_frame)
-
-    # Получаем текущий номер кадра
-    current_frame = input_video.get(cv2.CAP_PROP_POS_FRAMES)
-    # Вычисляем процент завершения
-    progress = (current_frame / total_frames) * 100
-
-    # Выводим процесс перезаписи видео
-    print(f"Перезапись видео: {progress:.2f}%")
+    out.write(frame)
 
 # Освобождаем ресурсы
-input_video.release()
-output_video.release()
-cv2.destroyAllWindows()
+cap.release()
+out.release()
