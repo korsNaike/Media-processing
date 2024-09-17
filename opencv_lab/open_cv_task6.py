8268319440e08c2905fb95975ff28e0ac0fef3d8
korsNaike <kirillgrinev2003@mail.ru>
2024-09-17 16:54:44 +0300

    Л1.З6. Видео с крестом

--- opencv_lab/open_cv_task6.py
+++ opencv_lab/open_cv_task6.py
@@ -11,9 +11,6 @@ while True:
         print("Не удалось захватить изображение с камеры.")
         break
 
-    # Применение размытия к кадру
-    blurred_frame = cv2.GaussianBlur(frame, (15, 15), 0)
-
     # Получение размеров кадра
     height, width, _ = frame.shape
 
@@ -24,23 +21,48 @@ while True:
     cross_width = 150
     cross_height = 20
 
-    # Рисуем пустые прямоугольники (крест)
-    # Горизонтальная линия
-    cv2.rectangle(blurred_frame,
+    # Создание маски для креста
+    mask = np.zeros((height, width), dtype=np.uint8)
+
+    # Рисуем пустые прямоугольники (кросс) в маске
+    cv2.rectangle(mask,
                   (center_x - cross_width//2, center_y - cross_height//2),
                   (center_x + cross_width//2, center_y + cross_height//2),
-                  (0, 0, 255),  # Красный цвет
-                  2)  # Толщина линии
-
-    # Вертикальная линия
-    cv2.rectangle(blurred_frame,
+                  (255),  # Белый цвет для области креста
+                  -1)  # Заполнить
+    cv2.rectangle(mask,
                   (center_x - cross_height//2, center_y - cross_width//2),
                   (center_x + cross_height//2, center_y + cross_width//2),
+                  (255),  # Белый цвет для области креста,
+                  -1)  # Заполнить
+
+    # Применение размытия к кадру
+    blurred_frame = cv2.GaussianBlur(frame, (15, 15), 0)
+
+    # Инвертируем маску, чтобы размыть всё, кроме креста
+    inverted_mask = cv2.bitwise_not(mask)
+
+    # Применяем маску к размытости и оригинальному изображению
+    blurred_background = cv2.bitwise_and(blurred_frame, blurred_frame, mask=mask)
+    foreground = cv2.bitwise_and(frame, frame, mask=inverted_mask)
+
+    # Складываем размытое фоновое изображение и крест
+    result = cv2.add(blurred_background, foreground)
+
+    # Рисуем красные границы на кресте
+    cv2.rectangle(result,
+                  (center_x - cross_width // 2, center_y - cross_height // 2),
+                  (center_x + cross_width // 2, center_y + cross_height // 2),
+                  (0, 0, 255),  # Красный цвет
+                  2)  # Толщина линии
+    cv2.rectangle(result,
+                  (center_x - cross_height // 2, center_y - cross_width // 2),
+                  (center_x + cross_height // 2, center_y + cross_width // 2),
                   (0, 0, 255),  # Красный цвет
                   2)  # Толщина линии
 
     # Отображение результата
-    cv2.imshow("Красный крест", blurred_frame)
+    cv2.imshow("Red Cross", result)
 
     # Выход при нажатии клавиши 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
